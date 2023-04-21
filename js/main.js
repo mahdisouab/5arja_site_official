11663b8781e0db2ba3980dd3cffe62097659162c
mahdisouab <97970543+mahdisouab@users.noreply.github.com>
2023-04-21 22:49:15 +0100

    nav bar mobile done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-const co
+const collapsible = document.querySelectorAll(".collapsible");
+collapsible.forEach((item) =>
+    item.addEventListener("click", function() {
+        this.classList.toggle("collapsible--expanded")
+    })
+);
